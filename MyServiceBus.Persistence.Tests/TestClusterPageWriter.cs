0a8005f2c048f109dd57bb8a5b436e9574e9680c
Andrey <amigin@gmail.com>
2021-05-21 12:20:04 +0300

    Fixed Unit Test with saving And Restoring Compressed content

--- MyServiceBus.Persistence.Tests/TestClusterPageWriter.cs
+++ MyServiceBus.Persistence.Tests/TestClusterPageWriter.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MyAzurePageBlobs;
 using MyServiceBus.Persistence.AzureStorage.CompressedMessages;
 using MyServiceBus.Persistence.Domains.MessagesContent;
 using MyServiceBus.Persistence.Domains.MessagesContentCompressed;
+using MyServiceBus.Persistence.Grpc;
 using NUnit.Framework;
 
 namespace MyServiceBus.Persistence.Tests
@@ -10,13 +13,25 @@ namespace MyServiceBus.Persistence.Tests
     public class TestClusterPageWriter
     {
 
-        private async Task TestPageWriteAndRead(PagesCluster clusterPage, long pageId)
+        private async Task TestPageWriteAndRead(PagesCluster clusterPage, long pageId, Dictionary<long, byte[]> cache)
         {
             var messagePageId = new MessagePageId(pageId);
             var hasPage = await clusterPage.HasPageAsync(messagePageId);
             Assert.IsFalse(hasPage);
+
+
+            var msg = new MessageContentGrpcModel
+            {
+                Created = DateTime.UtcNow,
+                Data = new [] {(byte)pageId, (byte)pageId, (byte)pageId},
+                MessageId = pageId,
+
+            };
             
-            var content = new CompressedPage(new [] {(byte)pageId, (byte)pageId, (byte)pageId});
+            var content = new CompressedPage(new []{msg});
+            
+            cache.Add(pageId, content.ZippedContent.ToArray());
+            
             await clusterPage.WriteAsync(messagePageId, content);
             hasPage = await clusterPage.HasPageAsync(messagePageId);
             Assert.IsTrue(hasPage);
@@ -27,12 +42,12 @@ namespace MyServiceBus.Persistence.Tests
         }
 
 
-        private async Task DoubleCheck(PagesCluster clusterPage, long pageId)
+        private async Task DoubleCheck(PagesCluster clusterPage, long pageId, byte[] srcContent)
         {
             var messagePageId = new MessagePageId(pageId);
-            var content = new CompressedPage(new [] {(byte)pageId, (byte)pageId, (byte)pageId});
             var resultContent = await clusterPage.ReadAsync(messagePageId);
-            resultContent.ZippedContent.AssertAllBytesAreEqualWith(content.ZippedContent);
+            Assert.AreEqual(resultContent.Messages[0].MessageId, pageId);
+            resultContent.ZippedContent.AssertAllBytesAreEqualWith(srcContent);
         }
 
 
@@ -45,15 +60,18 @@ namespace MyServiceBus.Persistence.Tests
             var clusterPage = new PagesCluster(azurePageBlob, new ClusterPageId(0), "test");
 
 
+            var dict = new Dictionary<long, byte[]>();
+            
+            
             for (var i = 0; i < 99; i++)
             {
-                await TestPageWriteAndRead(clusterPage, i);
+                await TestPageWriteAndRead(clusterPage, i, dict);
             }
             
             
             for (var i = 0; i < 99; i++)
             {
-                await DoubleCheck(clusterPage, i);
+                await DoubleCheck(clusterPage, i, dict[i]);
             }
 
     
